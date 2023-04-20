2d6f07cb88ee0c956f43fbfcc6ccd4d8e1a97182
b.bastieri <b.bastieri@live.com>
2023-04-20 16:27:03 -0300

    desafio2

---
+++ desafio2.js
@@ -0,0 +1,126 @@
+const fs = require ('fs');
+
+class ProductManager {
+
+    constructor () {
+        this.path = './products.json'
+    }
+
+    async getProducts (){
+        try {
+            if (fs.existsSync(this.path)){
+                const products = await fs.promises.readFile(this.path, 'utf8');
+                const productsJS = JSON.parse(products);
+                return productsJS;
+            } else{
+                return [];
+            }
+
+        } catch (error){
+            console.log(error)
+        }
+    }
+
+    async addProduct (title, description, price, thumbnail, code, stock) {
+        try {
+            const productsList = await this.getProducts();
+            const productCode = productsList.find((product) => product.code === code);
+            if (productCode) {
+                console.log ('This product already exists.')
+            } else{
+                const lastProd = productsList[productsList.length - 1];
+                const newID = productCode ? lastProd.id +1 : 1;
+                const product = {
+                    id : newID,
+                    title,
+                    description,
+                    price,
+                    thumbnail,
+                    code,
+                    stock,
+            }
+            productsList.push(product);
+            await fs.promises.writeFile(this.path, JSON.stringify(productsList));
+            }
+
+        } catch (error){
+        console.log(error);
+        } 
+    }
+
+    async deleteProduct(idProduct) {
+        try {
+          const productsList = await this.getProducts();
+          const idDelete = productsList.findIndex((product) => product.id === idProduct);
+        
+          if(idDelete < 0){
+            console.log ('ID not found');
+          } else{
+            const index = productsList.indexOf(idDelete)
+            productsFile.splice(index, 1);
+            await fs.promises.writeFile(this.path, JSON.stringify(productsList));
+            console.log(`Deleted product: ID "${idProduct}"`);
+          } 
+        } catch (error){
+            console.log(error)
+        }  
+    }
+
+    async updateProduct (idProduct, toBeUpdated) {
+        try{
+          const productsList = await this.getProducts();
+          const idUpdate = productsList.findIndex ((product) => product.id === idProduct);
+          
+          if (idUpdate < 0){
+            console.log ('ID not found');
+          } else {
+            const productUpdated = {
+                ...productsFile[idUpdate],
+                ...toBeUpdated,
+                id: idProduct,
+            };
+            productsFile[idUpdate] = productUpdated;
+            await fs.promises.writeFile(this.path, JSON.stringify(productsList));
+            console.log(`Product ID "${idProduct}" successfully updated`);    
+          }
+        } catch (error){
+            console.log(error);
+        }
+    }
+
+    async getProductByID (idProduct){
+        try {
+          const productsList = await this.getProducts();
+          const productByID = productsList.find(product => product.id === idProduct);
+          return productByID;   
+
+        } catch (error){
+            console.log(error);
+        }
+    }
+
+}
+
+const productManager = new ProductManager();
+
+const product1 = {
+    title: 'Bombi Valkyria',
+    description: 'Bombi tiro alto',
+    price: 2000,
+    thumbnail: 'bombivalkyria.jpg',
+    code: 'bombivalkyria',
+    stock:5,
+}
+
+const test = async () => {
+    const get1 = await productManager.getProducts();
+    console.log('First query:', get1);
+    await productManager.addProduct(product1);
+    const get2 =  await productManager.getProducts();
+    console.log('Second query:', get2)
+    const get3 = await productManager.getProductByID(1);
+    console.log ('Product:', get3);
+    await productManager.deleteProduct(2);
+}
+
+test();
