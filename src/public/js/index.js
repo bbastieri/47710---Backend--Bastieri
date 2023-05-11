526a29ad7657c9824d2867f7cdb1c19c6a5ee3db
b.bastieri <b.bastieri@live.com>
2023-05-11 15:15:05 -0300

    tercer commit

---
+++ src/public/js/index.js
@@ -0,0 +1,8 @@
+const socketClient = io();
+
+const form = document.getElementById('form');
+const inputId = document.getElementById('id')
+const inputName = document.getElementById('name');
+const inputDescription = document.getElementById('description');
+const inputCode = document.getElementById('code');
+const inputPrice = document.getElementById('price');
